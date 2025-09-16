cde631e717972493938dc8d1407ad15b0864cfd3
verticul <kelvinakinolaajayi@hotmail.com>
2025-09-16 16:20:30 +0100

    Making the operation asychronous

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,9 +7,10 @@ import {
 } from '../common.js';
 import renderError from './Error.js';
 import renderSpinner from './Spinner.js';
+import renderJobList from './JobList.js';
 
 // -- SEARCH COMPONENT
-const submitHandler = event => {
+const submitHandler = async event => {
     // Prevent default submit form behaviour
     event.preventDefault();
 
@@ -34,51 +35,31 @@ const submitHandler = event => {
     renderSpinner('search');
 
     // Fetch the search results
-    fetch(`${BASE_API_URL}/jobs?search=${searchText}`)
-        .then( response => {
-            if (!response.ok) {
-                console.log('Something went wrong');
-                return;
-            }
+    try {
+        const response = await fetch(`${BASE_API_URL}/jobs?search=${searchText}`);
+        const data = await response.json();
 
-            return response.json();
-        })
-        .then(data => {
-            // Extract job items only
-            const { jobItems } = data;
+        if (!response.ok) { // 4xx, 5xx status code
+            throw new Error(data.description);
+        }
 
-            // Remove the spinner
-            renderSpinner('search');
+         // Extract job items only
+        const { jobItems } = data;
 
-            // Render the number of results
-            numberEl.textContent = jobItems.length;
+        // Remove the spinner
+        renderSpinner('search');
 
-            // Render job items in search job list
-            jobItems.slice(0, 7).forEach(jobItem => {
-                const newJobItemHTML = `
-                <li class="job-item">
-                    <a class="job-item__link" href="${jobItem.id}">
-                        <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                        <div class="job-item__middle">
-                            <h3 class="third-heading">${jobItem.title}</h3>
-                            <p class="job-item__company">${jobItem.company}</p>
-                            <div class="job-item__extras">
-                                <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i>${jobItem.duration}</p>
-                                <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salary}</p>
-                                <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i>${jobItem.location}</p>
-                            </div>
-                        </div>
-                        <div class="job-item__right">
-                            <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                            <time class="job-item__time">${jobItem.daysAgo}d</time>
-                        </div>
-                    </a>
-                </li>
-                `;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-            });
-        })
-        .catch(error => console.log(error));
+        // Render the number of results
+        numberEl.textContent = jobItems.length;
+
+        // Render job items in search job list
+        renderJobList(jobItems);
+    } catch (error) {
+        renderSpinner('search');
+        renderError(error.message);
+    }
 };
 
 searchFormEl.addEventListener('submit', submitHandler);
+
+export default renderError;
